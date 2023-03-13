7ca3f12be41ca677df6913549ab7d633831bc635
yuxuzi <yuxuzi@Plum>
2023-03-12 20:13:09 -0400

    makeing navigation resuable

--- src/App.js
+++ src/App.js
@@ -1,32 +1,25 @@
 import "./App.css";
-import {GoBell, GoCloudDownload, GoDatabase} from "react-icons/go";
-import Button from "./Buttons";
+import Sidebar from "./components/Sidebar";
+import Route from "./components/Route";
+import AccordionPage from "./pages/AccordionPage";
+import DropdownPage from "./pages/DropdownPage";
+import ButtonsPage from "./pages/ButtonsPage";
 
 function App() {
-  return (
-    <div className="App">
-      <div>
-        <Button secondary outline rounded>
-          <GoBell />
-          Click me!!
-        </Button>
-      </div>
-      <div>
-        <Button danger outline>
-          <GoCloudDownload />
-          Buy Now!
-        </Button>
-      </div>
-      <div>
-        <Button warning>See Deal!
-          <GoDatabase />
-        </Button>
-      </div>
-      <div>
-        <Button></Button>
-      </div>
-      <div>
-        <Button></Button>
+
+  return  (
+    <div className="container mx-auto grid grid-cols-6 gap-4 mt-4">
+      <Sidebar />
+      <div className="col-span-5">
+        <Route path="/accordion">
+          <AccordionPage />
+        </Route>
+        <Route path="/">
+          <DropdownPage />
+        </Route>
+        <Route path="/buttons">
+          <ButtonsPage />
+        </Route>
       </div>
     </div>
   );
