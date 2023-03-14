bbb24a825b9873d986eb78bff0b2ae2488134e90
yuxuzi <yuxuzi@Plum>
2023-03-13 23:03:53 -0400

    use reducer

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import DropdownPage from './pages/DropdownPage';
 import ButtonPage from './pages/ButtonsPage';
 import ModalPage from './pages/ModalPage';
 import TablePage from './pages/TablePage';
-
+import CounterPage from './pages/CounterPage';
 function App() {
   return (
     <div className="container mx-auto grid grid-cols-6 gap-4 mt-4">
@@ -26,7 +26,9 @@ function App() {
         <Route path="/table">
           <TablePage />
         </Route>
-
+        <Route path="/counter">
+          <CounterPage initialCount={10} />
+        </Route>
   
       </div>
     </div>
